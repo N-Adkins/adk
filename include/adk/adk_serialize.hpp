e24606269f961bc44e7f41a83d5904f1a023f13f
N-Adkins <noahcadkins@gmail.com>
2024-03-24 17:03:44 -0400

    Began adk_serialize.hpp

---
+++ include/adk/adk_serialize.hpp
@@ -0,0 +1,29 @@
+/*  
+    ADK_Serialize - v0.1
+    No warranty implied, use at your own risk.
+    
+    Requires adk_reflect.hpp to be in the same directory.
+
+    Serialization for builtin types and structures with reflection metadata
+    from adk_reflect.hpp.
+
+    STD containers and types are not supported yet.
+
+    The public interface is adk::serialize, adk::serialize::internal should not be accessed
+    unless you know what you're doing.
+*/
+
+#ifndef ADK_SERIALIZE_HPP
+#define ADK_SERIALIZE_HPP
+
+namespace adk::serialize::internal
+{
+
+} // namespace adk::serialize::internal
+
+namespace adk::serialize
+{
+
+} // namespace adk::serialize
+
+#endif
